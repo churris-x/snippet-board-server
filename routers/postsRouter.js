39ac81eca1db5d49c98cb1390061790045d4708c
Fran <francisco@altimari.nl>
2022-04-21 11:22:28 +0200

    Add user snippet POST & FETCH endpoints

--- routers/postsRouter.js
+++ routers/postsRouter.js
@@ -6,25 +6,25 @@ const { user, post, sequelize } = require("../models");
 // PATH	/posts
 const router = new Router();
 
-router.get('/', async (request, response) => {
-	try {
-		const allArtworks = await artwork.findAll({
-			// attributes: {
-			// 	include: [
-			// 		[sequelize.fn("COUNT", sequelize.col("bids")), "bidCount"]
-			// 	]
-			// },
-			// include: {
-			// 	model: bid,
-			// 	attributes: []
-			// },
-			// group: ['artwork.id'],
-		});
-		response.send(allArtworks);
-	} catch (error) {
-		response.status(500).send(`Something went wrong: ${error.message}`);
-	}
-});
+// router.get('/', async (request, response) => {
+// 	try {
+// 		const allPosts = await post.findAll({
+// 			// attributes: {
+// 			// 	include: [
+// 			// 		[sequelize.fn("COUNT", sequelize.col("bids")), "bidCount"]
+// 			// 	]
+// 			// },
+// 			// include: {
+// 			// 	model: bid,
+// 			// 	attributes: []
+// 			// },
+// 			// group: ['post.id'],
+// 		});
+// 		response.send(allPosts);
+// 	} catch (error) {
+// 		response.status(500).send(`Something went wrong: ${error.message}`);
+// 	}
+// });
 
 router.get('/user', auth, async (request, response) => {
 	const { id } = request.user;
@@ -32,21 +32,34 @@ router.get('/user', auth, async (request, response) => {
 	try {
 		const userPosts = await post.findAll({
 			where: { userId: id },
-			// attributes: {
-			// 	include: [
-			// 		[sequelize.fn("COUNT", sequelize.col("bids")), "bidCount"]
-			// 	]
-			// },
-			// include: {
-			// 	model: bid,
-			// 	attributes: []
-			// },
-			// group: ['post.id'],
 		});
+		response.send(userPosts);
 	} catch (error) {
 		response.status(500).send(`Something went wrong: ${error.message}`);
 	}
+});
 
+router.post('/user', auth, async (request, response) => {
+	const { id } = request.user;
+	const {
+		title,
+		body,
+		syntax
+	} = request.body;
+
+	if (!title || !body) return response.status(400).send('Missing title or body');
+
+	try {
+		const newPost = await post.create({
+			title,
+			body,
+			syntax,
+			userId: id,
+		});
+		response.send(newPost);
+	} catch (error) {
+		response.status(500).send(`Something went wrong: ${error.message}`);
+	}
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const { PORT } = require('./constants');
 
 const usersRouter = require('./routers/usersRouter');
-// const postsRouter = require('./routers/postsRouter');
+const postsRouter = require('./routers/postsRouter');
 
 const app = express();
 
@@ -14,5 +14,6 @@ app.use(jsonParser);
 
 app.get('/', (request, response) => response.send('Hello world!'));
 app.use('/users', usersRouter);
+app.use('/posts', postsRouter);
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
